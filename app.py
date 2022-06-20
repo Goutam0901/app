08d4084548999ea1413f170ccec36a323655b126
Goutam Kapoor <106803231+Goutam0901@users.noreply.github.com>
2022-06-20 16:05:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import pickle
+from flask import Flask, render_template, request
+
+app= Flask(__name__)
+loaded_model= pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('form.html')
+
+
+@app.route('/prediction',methods=["POST"])
+def prediction():
+    battery_power = request.form['batterypower']
+    int_memory = request.form['int_memory']
+    ram = request.form['ram']
+
+    prediction=loaded_model.predict([[battery_power,int_memory,ram]])
+    if prediction[0]==0:
+        prediction = "Low Cost"
+        
+    elif prediction==1:
+        prediction = "Medium Cost"
+        
+    elif prediction==2:
+        prediction = "High Cost"
+        
+    else:        
+        prediction = "Very High Cost"        
+ 
+    
+    return render_template('form.html', api_output=prediction)
+    
+app.run(debug=True)
